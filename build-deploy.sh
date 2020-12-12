6e4bd976ae6f6ca95d16a9c4cc5aa492c52c52f8
Wasis Putranto <wasis.putranto@binance.com>
2020-12-12 20:32:32 +0800

    Update build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 TAG=$(git rev-parse --short HEAD)
-docker build -t wputra/sinatra:$TAG .
-docker tag wputra/sinatra:$TAG wputra/sinatra:latest
 
-docker-compose up -d
+build () {
+  docker build -t wputra/sinatra:$TAG .
+  docker tag wputra/sinatra:$TAG wputra/sinatra:latest
+}
+
+deploy () {
+  docker-compose up -d
+}
+
+migrate () {
+  docker-compose run web bundle exec rake db:schema:load
+}
+
+build
+deploy
