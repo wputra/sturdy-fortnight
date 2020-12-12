31cf2049de2669fb1b65789bb76d0e41ffdf978d
Wasis Putranto <wasis.putranto@binance.com>
2020-12-12 10:23:34 +0800

    use ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM ruby:2.7
 WORKDIR /app
 COPY app .
-RUN apt-get install libpq-dev && \
-    bundle install
+RUN bundle install
 EXPOSE 4567
 CMD ["bundle", "exec", "ruby", "app.rb"]
--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/activerecord'
 
-set :database_file, 'database.yml'
+#set :database_file, 'database.yml'
 set :bind, '0.0.0.0'
 
 get '/' do
