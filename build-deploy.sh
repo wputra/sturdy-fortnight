1d0c078859b907a37da1feea5c15dfd81c217521
Wasis Putranto <wasis.putranto@binance.com>
2020-12-12 21:37:13 +0800

    sinatra

--- build-deploy.sh
+++ build-deploy.sh
@@ -14,11 +14,13 @@ deploy () {
 
 db_migrate () {
   echo "migrating database..."
+  docker-compose run web bundle exec rake db:create
   docker-compose run web bundle exec rake db:migrate
 }
 
 db_load () {
   echo "loading database..."
+  docker-compose run web bundle exec rake db:create
   docker-compose run web bundle exec rake db:schema:load
 }
 
